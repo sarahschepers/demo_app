8fd8d01b2e74e8490c896c859a3e378b814bfc96
Sarah Schepers <1033142@student.khlim.be>
2013-11-28 14:13:56 +0100

    Validates toegevoegd

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
   has_many :microposts
+  validates_presence_of :name,{presence => true}
+  validates_presence_of :email,{presence => true}
+  validates :email, :uniqueness => true
+  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
 end
