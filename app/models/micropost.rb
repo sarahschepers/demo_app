51409d8822fa4c38780ad94a010f16bf1227133c
Sarah Schepers <1033142@student.khlim.be>
2013-11-28 12:43:32 +0100

    Finish

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,5 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
+  validates_length_of :content,{:maximum => 25, :minimum => 8,}
+  validates_presence_of :user_id,{presence => true}
 end
